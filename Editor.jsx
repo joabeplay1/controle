91557291297728010bffd5fdd50ca5286f977582
joabeplay1 <joabejonatas186@gmail.com>
2026-06-01 14:13:17 -0300

    Delete Editor.jsx

--- Editor.jsx
+++
@@ -1,5 +0,0 @@
-// Editor completo de apps gerados
-// Modos: Preview | Código | Split
-// Painéis: PreviewPanel, CodeEditor, ChatPanel
-// Features: salvar (Ctrl+S), histórico de versões, exportar
-// Usa: base44.entities.Project.update
